0dad5eca5c4cd1eb258ee60f36e4d2384e404498
andralaiskodat <andraqbz@gmail.com>
2024-05-30 14:52:52 +0700

    Setup Prak 8

---
+++ vue-project-praktikum-8/src/components/Artikel.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Ini Adalah Komponen Artikel</h1>
+        <p>ini adalah isi paragraf</p>
+    </div>
+    <div>
+        <img src="../assets/omen.gif" alt="" width="300px"
+    </div>
+</template>
